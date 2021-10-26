8e9fdd7d2c53e29ef2d785d5903190a8d79e7f7b
zlilyoffe <zlilyoffe@gmail.com>
2021-10-26 22:05:31 +0300

    syntax-onChange

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './Input.css';
 
-const Input = ({ massege, setMessage, sendMessage }) => (
+const Input = ({ message, setMessage, sendMessage }) => (
     <form className="form">
         <input
         className="input"
         type="text"
         placeholder="Type a message..."
-        value={massege}
+        value={message}
         onChange={(event) => setMessage(event.target.value)}
         onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
         />
