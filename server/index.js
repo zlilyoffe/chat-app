8fd7bca5cf1833f0e592cc3843aff61cc5b2be4f
zlilyoffe <zlilyoffe@gmail.com>
2021-10-26 21:52:20 +0300

    add cors to index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require ('http');
 const express = require ('express');
 const socketio = require ('socket.io');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
